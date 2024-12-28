c9e148bcaf01fecadabfefa8af8b3da6cd1a6680
Julian Schöpe <j.schoepe@ostfalia.de>
2024-12-28 14:53:30 +0100

    trigger scheduled task instantly on startup/edit

--- django_server/fpf_sensor_service/sensors/__init__.py
+++ django_server/fpf_sensor_service/sensors/__init__.py
@@ -10,8 +10,3 @@ To disable any Sensor just comment out the line by putting a # in front of it
 from .dht22_sensors import PinDHT22HumiditySensor, PinDHT22TemperatureSensor
 
 
-
-"""
-THIS HAS TO BE AT THE BOTTOM OF THE FILE BELOW ALL THE SENSOR CLASS IMPORTS TO FUNCTION!!!
-IT REPRESENTS A SINGLETON THAT SHOULD ALWAYS BE ACCESSED FROM HERE
-"""
